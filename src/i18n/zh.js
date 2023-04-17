63ac517be97525eb33bb27a5225f061dfb955526
Alan Wang <wyj@tbird.cn>
2023-04-17 19:47:15 +0800

    修改welcome

--- src/i18n/zh.js
+++ src/i18n/zh.js
@@ -5,7 +5,7 @@ const zh = {
   synapseadmin: {
     auth: {
       base_url: "服务器 URL",
-      welcome: "欢迎来到 Synapse-admin",
+      welcome: "管理后台",
       server_version: "Synapse 版本",
       username_error: "请输入完整有效的用户 ID: '@user:domain'",
       protocol_error: "URL 需要以'http://'或'https://'作为起始",
